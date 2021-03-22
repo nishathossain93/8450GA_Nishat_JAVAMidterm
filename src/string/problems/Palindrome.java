f9f2e3e3147dfb44927a5fa230c392c5e04f472e
= <=>
2021-03-21 17:22:38 -0700

    Finished Palindrome.java in string.problems

--- src/string/problems/Palindrome.java
+++ src/string/problems/Palindrome.java
@@ -10,5 +10,23 @@ public class Palindrome {
             Write a method to check if a given String is a palindrome or not.
          */
 
+
+        checkPalindrome("Madam");
+
+    }
+
+
+    public static void checkPalindrome(String word) {
+        String actual = word.toLowerCase();
+        String reverse = " ";
+
+        for (int i = actual.length() - 1; i >= 0; i--) {
+            reverse = reverse + actual.charAt(i);
+        }
+        if (actual.equals(reverse)) {
+            System.out.println("is palindrome");
+        } else {
+            System.out.println("Not a palindrome");
+        }
     }
 }
