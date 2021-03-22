d4ceb08f3aebe5ae41ecb43182adc42a32d45885
= <=>
2021-03-21 17:31:20 -0700

    Finished Permutation.java in string.problems

--- src/string/problems/Permutation.java
+++ src/string/problems/Permutation.java
@@ -9,5 +9,19 @@ public class Permutation {
 
         Write Java program to compute all permutations of any given String
          */
+
+        permutation(" ","ABC");
     }
+
+    public static void permutation(String prefix, String  str) {
+        int n = str.length();
+        if (n == 0) {
+            System.out.println(prefix);
+        } else {
+            for (int i = 0; i < n; i++) {
+                permutation(prefix + str.charAt(i), str.substring(0, i) + str.substring(i + 1, n));
+            }
+        }
+    }
+
 }
